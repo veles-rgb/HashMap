e2456014470b47a9156f92b71ac7c1ef036da6cc
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-05-14 20:10:24 -0400

    Remove unused methods from LinkedList class

--- linkedList.js
+++ linkedList.js
@@ -18,12 +18,8 @@ class LinkedList {
 
     prepend(key, value = null) {
         const node = new Node(key, value);
-        if (this.head === null) {
-            this.head = node;
-        } else {
-            node.nextNode = this.head;
-            this.head = node;
-        }
+        node.nextNode = this.head;
+        this.head = node;
     }
 
     size() {
@@ -36,19 +32,6 @@ class LinkedList {
         return count;
     }
 
-    getHead() {
-        return this.head === null ? "Empty list!" : this.head;
-    }
-
-    getTail() {
-        if (this.head === null) return "Empty list";
-        let currentNode = this.head;
-        while (currentNode.nextNode !== null) {
-            currentNode = currentNode.nextNode;
-        }
-        return currentNode;
-    }
-
     at(index) {
         let currentNode = this.head;
         let i = 0;
@@ -57,33 +40,6 @@ class LinkedList {
             i++;
         }
         return currentNode;
-    }
-
-    pop() {
-        if (this.size() <= 1) {
-            this.head = null;
-            return;
-        }
-        let currentNode = this.head;
-        let index = 1;
-        const beforeLast = this.size() - 1;
-        while (index !== beforeLast) {
-            currentNode = currentNode.nextNode;
-            index++;
-        }
-        console.log(`Removed "${currentNode.nextNode.key}"`);
-        currentNode.nextNode = null;
-    }
-
-    contains(key) {
-        let currentNode = this.head;
-        while (currentNode !== null) {
-            if (currentNode.key === key) {
-                return true;
-            }
-            currentNode = currentNode.nextNode;
-        }
-        return false;
     }
 
     find(key) {
@@ -97,40 +53,6 @@ class LinkedList {
             index++;
         }
         return null;
-    }
-
-    toString() {
-        let listString = "";
-        let currentNode = this.head;
-        if (this.head === null) return "Empty list!";
-        while (currentNode !== null) {
-            listString += `(${currentNode.key}${currentNode.value !== null ? ", " + currentNode.value : ""}) -> `;
-            currentNode = currentNode.nextNode;
-        }
-        return listString + "null";
-    }
-
-    insertAt(key, index, value = null) {
-        if (index < 0 || index > this.size()) {
-            return `Invalid index. Please use 0-${this.size()}.`;
-        }
-        const newNode = new Node(key, value);
-        if (index === 0) {
-            newNode.nextNode = this.head;
-            this.head = newNode;
-            return `Added ${key} to index 0.`;
-        }
-        let currentNode = this.head;
-        let previousNode = null;
-        let i = 0;
-        while (i < index) {
-            previousNode = currentNode;
-            currentNode = currentNode.nextNode;
-            i++;
-        }
-        previousNode.nextNode = newNode;
-        newNode.nextNode = currentNode;
-        return `Added ${key} to index ${index}.`;
     }
 
     removeAt(index) {
