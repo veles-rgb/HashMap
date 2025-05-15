7d2a01ce96150c61d3971eeface3bd9ce437c4a0
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-05-15 14:00:33 -0400

    Add tests for new HashSet class

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
 // main.js
 import { HashMap } from "./hashMap.js";
+import { HashSet } from "./hashSet.js";
 
-const test = new HashMap(0.75, 16);
+// HashMap test //
+const mapTest = new HashMap();
 
-test.set('apple', 'red');
-test.set('banana', 'yellow');
-test.set('carrot', 'orange');
-test.set('dog', 'brown');
-test.set('elephant', 'gray');
-test.set('frog', 'green');
-test.set('grape', 'purple');
-test.set('hat', 'black');
-test.set('ice cream', 'white');
-test.set('jacket', 'blue');
-test.set('kite', 'pink');
-test.set('lion', 'golden');
+mapTest.set('apple', 'red');
+mapTest.set('banana', 'yellow');
+mapTest.set('carrot', 'orange');
+mapTest.set('dog', 'brown');
+mapTest.set('elephant', 'gray');
+mapTest.set('frog', 'green');
+mapTest.set('grape', 'purple');
+mapTest.set('hat', 'black');
+mapTest.set('ice cream', 'white');
+mapTest.set('jacket', 'blue');
+mapTest.set('kite', 'pink');
+mapTest.set('lion', 'golden');
+mapTest.set('moon', 'silver');
 
-test.set('moon', 'silver');
-console.log(test.entries());
+// HashSet test //
+
+const setTest = new HashSet();
+
+setTest.set('apple', 'red');
+setTest.set('banana', 'yellow');
+setTest.set('carrot', 'orange');
+setTest.set('dog', 'brown');
+setTest.set('elephant', 'gray');
+setTest.set('frog', 'green');
+setTest.set('grape', 'purple');
+setTest.set('hat', 'black');
+setTest.set('ice cream', 'white');
+setTest.set('jacket', 'blue');
+setTest.set('kite', 'pink');
+setTest.set('lion', 'golden');
+setTest.set('moon', 'silver');
